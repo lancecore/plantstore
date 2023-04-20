43414b0b9eb365838e9ed917815d7149131ef6cb
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2023-04-19 21:43:44 -0700

    Single plant dynamic pages working

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,8 @@
 import Link from 'next/link';
-import plant from '../public/img/plant1.png';
-import Plants from '../components/Plants';
+import { plants } from '../components/Plants';
 
 export default function Card() {
-	const allPlants = Plants();
-	const plantCards = allPlants.map((plant, i) => (
+	const plantCards = plants.map((plant, i) => (
 		<Link href={`/shop/${plant.name}`} scroll={false} className="flex flex-col justify-between mt-20 py-6 px-6 rounded-xl shadow-lg hover:shadow border-2 border-transparent hover:border-newpurple-900 bg-newgreen-500 text-newpurple-950 text-center group transition-all ease-in-out duration-200" key={i}>
 			<img src={`/img/${plant.imgFile}`} alt={`Photo of ${plant.name}`} className="-mt-20 h-64 w-auto mx-auto" />
 			<h3 className="my-4 font-semibold text-lg">{plant.name}</h3>
--- pages/shop/[name].js
+++ pages/shop/[name].js
@@ -1,39 +1,39 @@
 import Layout from '../../components/Layout';
-import Plants from '../../components/Plants';
-import { useRouter } from 'next/router';
+import { plants } from '../../components/Plants';
 import parse from 'html-react-parser';
 
-// https://nextjs.org/docs/basic-features/data-fetching/get-static-paths
-
-const PlantSingle = () => {
-	const router = useRouter();
-	const { name } = router.query;
-	const allPlants = Plants();
-	const thisPlant = name;
-	const filtered = allPlants.filter(plant => {
-		return plant.name === thisPlant;
+export const getStaticProps = async ({ params }) => {
+	const allPlants = plants.filter(plant => {
+		return plant.name === params.name;
 	});
-	const plant = filtered[0];
+	return {
+		props: {
+			plant: allPlants[0],
+		},
+	};
+};
 
+export const getStaticPaths = async () => {
+	const paths = plants.map(plant => ({
+		params: { name: plant.name },
+	}));
+	return { paths, fallback: false };
+};
+
+export default function SinglePlant({ plant }) {
 	return (
 		<Layout>
 			<main className="min-h-screen p-24">
-				{filtered[0] ? (
-					<>
-						<h1 className="w-full mb-12 ml-1 text-7xl font-bold text-newgreen-100">{plant.name}</h1>
-						<div className="grid grid-cols-2 gap-x-8 py-6 px-6 rounded-xl shadow-lg  border-2 border-newpurple-800 bg-newgreen-500 text-newpurple-950 text-center group transition-all ease-in-out duration-200 overflow-x-visible">
-							<img src={`/img/${plant.imgFile}`} alt={`Photo of ${plant.name}`} className="-mt-20 w-auto h-[40rem] mx-auto" />
-							<section className="flex flex-col justify-between">
-								<p>{parse(plant.longerDesc)}</p>
-								<p className="mb-4 font-bold tracking-widest text-5xl">{plant.price}</p>
-								<p className="button mt-6 flex-shrink mx-auto hover:bg-newpurple-950">ADD TO CART</p>
-							</section>
-						</div>
-					</>
-				) : null}
+				<h1 className="w-full mb-12 ml-1 text-7xl font-bold text-newgreen-100">{plant.name}</h1>
+				<div className="grid grid-cols-2 gap-x-8 py-6 px-6 rounded-xl shadow-lg  border-2 border-newpurple-800 bg-newgreen-500 text-newpurple-950 text-center group transition-all ease-in-out duration-200 overflow-x-visible">
+					<img src={`/img/${plant.imgFile}`} alt={`Photo of ${plant.name}`} className="-mt-20 w-auto h-[40rem] mx-auto" />
+					<section className="flex flex-col justify-between">
+						<p>{parse(plant.longerDesc)}</p>
+						<p className="mb-4 font-bold tracking-widest text-5xl">{plant.price}</p>
+						<p className="button mt-6 flex-shrink mx-auto hover:bg-newpurple-950">ADD TO CART</p>
+					</section>
+				</div>
 			</main>
 		</Layout>
 	);
-};
-
-export default PlantSingle;
+}
